df0dc63be76d7140ea2fddaceb4416dc986a6565
utkarshsundkar <utkarshsundkar1905@gmail.com>
2026-03-02 18:39:54 +0530

    fix: Add missing import in exercise routes

--- server/src/routes/exercise.routes.js
+++ server/src/routes/exercise.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserExercises, saveExercise, saveFocusExercise, updateExerciseProgress, getTotalReps, getWorkoutStats, saveWorkoutCompletion } from '../controllers/exercise.controller.js';
+import { getUserExercises, saveExercise, saveFocusExercise, updateExerciseProgress, getTotalReps, getWorkoutStats, saveWorkoutCompletion, getUserStatsProgress } from '../controllers/exercise.controller.js';
 
 const router = express.Router();
 
