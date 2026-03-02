2e2d31adf68b2ce7b45c5ac581913758dfeb68e1
utkarshsundkar <utkarshsundkar1905@gmail.com>
2026-03-02 19:42:44 +0530

    refactor: Remove unused startOfWeek variable

--- server/src/controllers/exercise.controller.js
+++ server/src/controllers/exercise.controller.js
@@ -361,8 +361,7 @@ const getUserStatsProgress = asyncHandler(async (req, res) => {
 
   const now = new Date();
   const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-  const startOfWeek = new Date(now);
-  startOfWeek.setDate(now.getDate() - now.getDay()); // Sunday
+
 
   // 1. Monthly Duration (minutes)
   const monthlyWorkouts = await Workout.find({
